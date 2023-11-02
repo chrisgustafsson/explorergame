6658a4ed424f433f8620c7d4fee7e3370e960271
chrisgustafsson <>
2023-11-02 18:25:07 +0000

    ADD startGame logic

--- run.py
+++ run.py
@@ -10,4 +10,13 @@ print('_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"')
 name = input("Please enter your name here: ")
 print(f'Welcome explorer {name}!')
 
-startGame = input("Are you ready to go on an historic adventure? yes/no")
+startGame = input("Are you ready to go on an historic adventure? yes/no ")
+if startGame == "yes":
+    print("Amazing! Looking forward to go down the adventure path with you!")
+    print("In this adventure game you will have the opportunity to go back!")
+    world = input("Visit the 'Vikings' or the ancient 'Roman Empire'?")
+elif startGame == "no":
+    print("Too bad, you're missing out! Refresh page to restart!")
+    quit()
+else:
+    print("Invalid response, try answering 'yes' or 'no'")
