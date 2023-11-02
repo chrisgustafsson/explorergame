15f702c02da368978a832e5995d4f41edf5aaba0
chrisgustafsson <>
2023-11-02 18:44:42 +0000

    UPDATE Input with \n

--- run.py
+++ run.py
@@ -7,14 +7,14 @@ print("╚══════╝╚═╝  ╚═╝╚═╝     ╚════
 
 print('_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"')
 
-name = input("Please enter your name here: ")
+name = input("Please enter your name here:\n")
 print(f'Welcome explorer {name}!')
 
 startGame = input("Ready to go on an historic adventure? 'yes' or 'no'")
 if startGame == "yes":
     print("Amazing! Looking forward to go down the adventure path with you!")
     print("In this adventure game you will have the opportunity to go back!")
-    world = input("Visit the 'Vikings' or the ancient 'Roman Empire'?")
+    world = input("Visit the 'Vikings' or the ancient 'Roman Empire'?\n")
 elif startGame == "no":
     print("Too bad, you're missing out! Refresh page to restart!")
     quit()
