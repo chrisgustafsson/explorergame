9e8ee7a6b0f75a078061e2207e2caf28191bdfd4
chrisgustafsson <>
2023-11-02 19:16:35 +0000

    ADD logic for vikings world start

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ print('_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"')
 name = input("Please enter your name here:\n")
 print(f'Welcome explorer {name}!')
 
-startGame = input("Ready to go on an historic adventure? 'yes' or 'no'")
+startGame = input("Ready to go on an historic adventure? 'yes' or 'no'\n")
 if startGame == "yes":
     print("Amazing! Looking forward to go down the adventure path with you!")
     print("In this adventure game you will have the opportunity to go back!")
@@ -22,3 +22,9 @@ else:
     print("Invalid response, try answering 'yes' or 'no'")
 
 print('_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"')
+
+if world == "vikings":
+    print("You see yourself transported to the far north of Europe.")
+    print("It's green and lucious during a beautiful spring day.")
+    print("In one direction you see a boat but the other direction is a farm.")
+    landingVikings = input("Take the 'boat' or go to the 'farm'?\n")
